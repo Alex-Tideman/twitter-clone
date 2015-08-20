d7d09558a510dce31d557dce92eca64cfd968375
Alex Tideman <alex.tideman@gmail.com>
2015-08-20 14:24:42 -0600

    Add user microposts

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -15,7 +15,7 @@ class UserMailerTest < ActionMailer::TestCase
   end
 
   test "password_reset" do
-    user = users(:michael)
+    user = users(:alex)
     user.reset_token = User.new_token
     mail = UserMailer.password_reset(user)
     assert_equal "Password reset", mail.subject
